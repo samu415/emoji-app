7f960da4c3d674556b97b872c17844c92ae5de10
Samuel <samu22@live.de>
2019-08-21 18:27:00 -0700

    Initial Commit

---
+++ Emoji App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emoji App
+//
+//  Created by Samuel Leicht on 2019-08-21.
+//  Copyright © 2019 Crap Tools. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
