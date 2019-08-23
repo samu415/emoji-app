b8036fcc0b4b17483f4cfaee58a395b5c0f6d3dd
Samuel <samu22@live.de>
2019-08-22 17:48:36 -0700

    Working on the 2nd screen

--- Emoji App/ViewController.swift
+++ Emoji App/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    var emojis = ["😃", "🐦", "🇨🇦", "🥳", "🥰", "🤓", "🤷🏻‍♂️"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
 }
-
